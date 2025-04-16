37647e7f4877a0d1434a6a9d14180dada3da791a
GilTorch <infotorch2014@gmail.com>
2025-04-16 09:47:40 -0400

    add amplify code'

---
+++ my-quizz-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "myquizzapp5ce2c3a8": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
